407813e4f7b77d0552c5acc54fed58945d37ac56
YoungRhee <nrcho0331@gmail.com>
2025-07-03 03:24:02 +0900

    07/02 수업 빠진 것 공부, page controller, rest controller

---
+++ src/main/java/com/example/sprbasic2025summer/controller/DefaultPageController.java
@@ -0,0 +1,59 @@
+package com.example.sprbasic2025summer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DefaultPageController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String index(){
+        return "index";
+    }
+    /*
+    @RequestMapping("/")
+    public String index2(){
+        return "index";
+    }
+    @RequestMapping("/index")
+    public String index3(){
+        return "index";
+    }*/
+
+    @RequestMapping("/aaa")
+    public String aaa(@RequestParam String name, @RequestParam String phone, Model model){
+        System.out.println("name : " + name);
+        System.out.println("phone : " + phone);
+        model.addAttribute("name", name);
+        model.addAttribute("phone", phone);
+        return "aaa";
+    }
+
+    @RequestMapping("/calculate")
+    public String calculate(String num1, String num2, Model model){
+        System.out.println("num1 : " + num1);
+        System.out.println("num2 : " + num2);
+        int int_num1 = Integer.parseInt(num1);
+        int int_num2 = Integer.parseInt(num2);
+        int sum = int_num1 + int_num2;
+
+        model.addAttribute("sum", sum);
+        return "calculate";
+    }
+
+    @RequestMapping("/calculate2")
+    public String calculate2(int num1, int num2, Model model){
+        int sum = num1 * num2;
+        model.addAttribute("sum", sum);
+        /*
+        Map<String, Object> temp = new HashMap<String, Object>();
+        temp.put("sum", sum);
+        temp.put("code", "success");
+        model.addAttribute("result", temp);
+        */
+        return "calculate";
+    }
+}
